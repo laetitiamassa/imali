7b21dff17b452d3b1d07977034c271829310aeaf
Laetitia Massa <laetitiamassa@gmail.com>
2015-02-21 19:42:47 +0100

    refined authorizations when not signed in

--- app/controllers/matters_controller.rb
+++ app/controllers/matters_controller.rb
@@ -1,4 +1,5 @@
 class MattersController < ApplicationController
+  before_filter :authenticate_user!
   before_action :set_matter, only: [:show, :edit, :update, :destroy]
   
 
--- app/controllers/prestations_controller.rb
+++ app/controllers/prestations_controller.rb
@@ -1,4 +1,5 @@
 class PrestationsController < ApplicationController
+  before_filter :authenticate_user!
   before_action :set_prestation, only: [:show, :edit, :update, :destroy]
 
   # GET /prestations
