38527fbf5c07b93e0748f910b87b2451d4bab079
Laetitia Massa <laetitiamassa@gmail.com>
2015-02-21 19:02:10 +0100

    design show views

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -29,7 +29,7 @@ class InvoicesController < ApplicationController
 
     respond_to do |format|
       if @invoice.save
-        format.html { redirect_to @invoice, notice: 'Invoice was successfully created.' }
+        format.html { redirect_to edit_invoice_path(@invoice), notice: 'Invoice was successfully created.' }
         format.json { render :show, status: :created, location: @invoice }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class InvoicesController < ApplicationController
   def update
     respond_to do |format|
       if @invoice.update(invoice_params)
-        format.html { redirect_to @invoice, notice: 'Invoice was successfully updated.' }
+        format.html { redirect_to invoices_path, notice: 'Invoice was successfully updated.' }
         format.json { render :show, status: :ok, location: @invoice }
       else
         format.html { render :edit }
--- app/models/prestation.rb
+++ app/models/prestation.rb
@@ -1,3 +1,7 @@
 class Prestation < ActiveRecord::Base
 	belongs_to :matter
+
+	def price
+		rate*amount
+	end
 end
